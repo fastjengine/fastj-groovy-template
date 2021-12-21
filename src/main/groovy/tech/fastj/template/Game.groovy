0118359666542d26310fa2920a95252fcaf937a5
lucasstarsz <andrewrcdey@gmail.com>
2021-12-21 13:46:33 -0500

    bumped fastj version to 1.6.0-SNAPSHOT-3

--- src/main/groovy/tech/fastj/template/Game.groovy
+++ src/main/groovy/tech/fastj/template/Game.groovy
@@ -1,7 +1,7 @@
 package tech.fastj.template
 
 import tech.fastj.engine.FastJEngine
-import tech.fastj.graphics.display.Display
+import tech.fastj.graphics.display.FastJCanvas
 import tech.fastj.graphics.display.RenderSettings
 import tech.fastj.graphics.game.Text2D
 import tech.fastj.systems.control.SimpleManager
@@ -9,18 +9,18 @@ import tech.fastj.systems.control.SimpleManager
 class Game extends SimpleManager {
 
     @Override
-    void init(Display display) {
+    void init(FastJCanvas canvas) {
         /* Some crispy anti-aliasing for the road. */
-        display.modifyRenderSettings(RenderSettings.Antialiasing.Enable)
+        canvas.modifyRenderSettings(RenderSettings.Antialiasing.Enable)
 
         /* A very simple Text2D object, welcoming you to FastJ! */
-        Text2D helloFastJText = Text2D.fromText('Hello, FastJ 1.5.0!')
-        helloFastJText.translate(display.getScreenCenter())
+        Text2D helloFastJText = Text2D.fromText('Hello, FastJ 1.6.0!')
+        helloFastJText.translate(canvas.getCanvasCenter())
         drawableManager.addGameObject(helloFastJText)
     }
 
     @Override
-    void update(Display display) {
+    void update(FastJCanvas canvas) {
     }
 
     static void main(String[] args) {

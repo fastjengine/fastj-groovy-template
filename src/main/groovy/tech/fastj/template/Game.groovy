37cade2d22ac7118feaddcbaf202279d2bba1bc0
lucasstarsz <andrewrcdey@gmail.com>
2022-08-16 01:55:41 -0400

    update to FastJ 1.7.0-SNAPSHOT-1 - improve jpackage speed by disabling installer generation - bump gradle version to 7.5.1 - bump slf4j to 2.0.0-alpha7 - improve example game - format readme - update license

--- src/main/groovy/tech/fastj/template/Game.groovy
+++ src/main/groovy/tech/fastj/template/Game.groovy
@@ -4,6 +4,7 @@ import tech.fastj.engine.FastJEngine
 import tech.fastj.graphics.display.FastJCanvas
 import tech.fastj.graphics.display.RenderSettings
 import tech.fastj.graphics.game.Text2D
+import tech.fastj.math.Pointf
 import tech.fastj.systems.control.SimpleManager
 
 class Game extends SimpleManager {
@@ -14,13 +15,14 @@ class Game extends SimpleManager {
         canvas.modifyRenderSettings(RenderSettings.Antialiasing.Enable)
 
         /* A very simple Text2D object, welcoming you to FastJ! */
-        Text2D helloFastJText = Text2D.fromText('Hello, FastJ 1.6.0!')
-        helloFastJText.translate(canvas.getCanvasCenter())
-        drawableManager.addGameObject(helloFastJText)
-    }
+        Text2D helloFastJ = Text2D.fromText("Hello, FastJ 1.7.0-SNAPSHOT-1!")
 
-    @Override
-    void update(FastJCanvas canvas) {
+        /* Translate our hello text to the center of the screen */
+        Pointf center = canvas.getCanvasCenter()
+        helloFastJ.setTranslation(center.subtract((float) (helloFastJ.width() / 2f), (float) (helloFastJ.height() / 2f)))
+
+        /* Render hello fastj text */
+        drawableManager().addGameObject(helloFastJ)
     }
 
     static void main(String[] args) {
